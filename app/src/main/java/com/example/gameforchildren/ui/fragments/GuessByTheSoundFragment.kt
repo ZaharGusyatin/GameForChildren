ab31a2a32bf497463c671fcd62a9fac8100e9b3c
zaharko1996 <redimerlives@gmail.com>
2020-10-13 17:12:06 +0300

    first commit

---
+++ app/src/main/java/com/example/gameforchildren/ui/fragments/GuessByTheSoundFragment.kt
@@ -0,0 +1,10 @@
+package com.example.gameforchildren.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gameforchildren.R
+
+class GuessByTheSoundFragment : Fragment(R.layout.fragment_guess_by_the_sound)
