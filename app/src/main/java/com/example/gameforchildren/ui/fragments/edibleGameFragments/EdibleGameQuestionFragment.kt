d48fb7068f1ccb96707e03d188a3bcff58d5801d
Vladimir Mudryakov <mudryakov21@mail.ru>
2020-10-16 16:42:15 +0300

    new onTouch

--- app/src/main/java/com/example/gameforchildren/ui/fragments/edibleGameFragments/EdibleGameQuestionFragment.kt
+++ app/src/main/java/com/example/gameforchildren/ui/fragments/edibleGameFragments/EdibleGameQuestionFragment.kt
@@ -5,6 +5,10 @@ import com.example.gameforchildren.model.FoodModel
 import com.example.gameforchildren.ui.fragments.LevelSelectionFragment
 import com.example.gameforchildren.utilits.*
 import kotlinx.android.synthetic.main.fragment_edible_game_question.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.logging.Handler
 
 
 class EdibleGameQuestionFragment : Fragment(R.layout.fragment_edible_game_question) {
@@ -18,35 +22,36 @@ class EdibleGameQuestionFragment : Fragment(R.layout.fragment_edible_game_questi
         initBaseActions()
         initFood()
         drawNewQuest()
+    startClick() //костыль
     }
 
     private fun initBaseActions() {
-                button_back_to_menu_from_edible.setOnClickListener {
+        button_back_to_menu_from_edible.setOnClickListener {
             replaceFragment(LevelSelectionFragment(), false)
         }
         APP_ACTIVITY.title = getString(R.string.EdibleGameTitle)
+
     }
 
     private fun resultFunc() {
         if (result) trueCount++
         result.progressChange(count)
         count++
-        if (count < rounds) {
+        if (count != rounds) {
             drawNewQuest()
-        } else endLevel(trueCount, rounds)
+        } else {
+            stopClick()  //костыль
+            endLevel(trueCount, rounds)
+        }
     }
-
-
     private fun drawNewQuest() {
-        result = false
+                result = false
         items = getRandomFood()
         edibleFirstItemName.text = items[0].name
         edibleSecondItemName.text = items[1].name
         edibleImageFirstItem.setImageResource(items[0].image)
         ediableImageSecondItem.setImageResource(items[1].image)
-        edibleImageFirstItem.isClickable = true
-        ediableImageSecondItem.isClickable = true
-         updateListeners()
+        updateListeners()
 
     }
 
@@ -62,4 +67,15 @@ class EdibleGameQuestionFragment : Fragment(R.layout.fragment_edible_game_questi
         edibleImageFirstItem.setOnTouchListener(listener1)
         ediableImageSecondItem.setOnTouchListener(listener2)
     }
+fun stopClick() { //времянка пока нет задержки
+    ediableImageSecondItem.isClickable = false
+    edibleImageFirstItem.isClickable = false
 }
+  fun startClick(){ //времянка пока нет задержки
+      ediableImageSecondItem.isClickable = true
+    edibleImageFirstItem.isClickable = true
+
+}
+}
+
+
